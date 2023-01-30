9a5a34e41574049ed93c06e6b98a86a4f023fe10
Ashish-Bopulkar <123931363+Ashish-Bopulkar@users.noreply.github.com>
2023-01-30 15:56:46 +0530

    first commit

---
+++ Day_1_Introduction-to-JS-Variables-and-Data-Types/problem.1.js
@@ -0,0 +1,7 @@
+//  Problem 1
+// Print "Masai School" in the console followed by "A Transformation in Education" in next line
+
+let a="Masai School";
+let b="A Transformation in Education";
+
+console.log(a+"\n"+b);
---
+++ Day_1_Introduction-to-JS-Variables-and-Data-Types/problem4.js
@@ -0,0 +1,36 @@
+//  Problem 4
+//  Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+//  Print the report card of the student (You can make it look nice by using some keyboard symbols )
+//  Explore ASCII ART (https://en.wikipedia.org/wiki/ASCII_art (Links to an external site.)) or Text Art (https://fsymbols.com/text-art/ (Links to an external site.)) for some inspiration
+
+// solution  -
+
+console.log("*************************************************");
+console.log("*                                               *")
+console.log("*          -- ｒｅｐｏｒｔ ｃａｒｄ --          *");
+console.log("*                                               *");
+console.log("*   Name - Ashish Bopulkar                      *");
+console.log("*                                               *");
+console.log("*   School - GNA Collage Dist Akola Maharastra  *");
+console.log("*                                               *");
+console.log("*   Grade - A+                                  *");
+console.log("*                                               *")
+console.log("*   Section - B                                 *")
+console.log("*                                               *")
+console.log("*   Roll No - 00112                             *")
+console.log("*                                               *")
+console.log("*   -----------------------------------------   *")
+console.log("*   |  Subject          |        Marks      |   *")
+console.log("*   |---------------------------------------|   *")
+console.log("*   |  English          |        89         |   *")
+console.log("*   |---------------------------------------|   *")
+console.log("*   |  Marathi          |        85         |   *")
+console.log("*   |---------------------------------------|   *")
+console.log("*   |  Hindi            |        87         |   *")
+console.log("*   |---------------------------------------|   *")
+console.log("*   |  Total            |     300/261       |   *")
+console.log("*   |---------------------------------------|   *")
+console.log("*   |  Final            |         Pass      |   *")
+console.log("*   -----------------------------------------   *")
+console.log("*                                               *")
+console.log("*************************************************")
