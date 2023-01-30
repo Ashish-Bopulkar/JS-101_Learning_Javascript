e8cfc6fc9d23dd37132f2c2d037706d4f8b0c986
Ashish-Bopulkar <123931363+Ashish-Bopulkar@users.noreply.github.com>
2023-01-30 16:27:12 +0530

    second commit's

---
+++ Day_2_MathematicalOprtor/problem1.js
@@ -0,0 +1,18 @@
+// Mathematical Oprtor
+
+// Solution :-
+
+let a=2;
+let b=3;
+
+let add = a+b ;
+let sub = a-b ;
+let mult= a*b ;
+let div = a/b ;
+let mod = a%b ;
+
+console.log("Addition is :-"+" "+add);
+console.log("Subsration is :-"+" "+sub);
+console.log("Multpliction is :-"+" "+mult);
+console.log("Division is :-"+" "+div);
+console.log("Mod is :-"+" "+mod);
---
+++ Day_2_MathematicalOprtor/problem2.js
@@ -0,0 +1,22 @@
+//  Problem2
+
+// Perform some oprtion's .
+
+// let a = 12;
+// let b = 13;
+// let c = 10;
+
+// console.log("ans is :- "+((b-a)+(b-c))**3);
+
+
+// calcu the square root of x ;
+
+// let x= 25;
+
+// console.log("Square root of x is :- "+(x**0.5));
+
+// let a = "hi";
+
+// console.log(a+x+typeof(a+x));
+
+console.log(3=="3");
