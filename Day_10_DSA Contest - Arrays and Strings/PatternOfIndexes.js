06617f6a42910961f8a4bd52647bdc9704771070
Ashish-Bopulkar <123931363+Ashish-Bopulkar@users.noreply.github.com>
2023-02-09 21:45:37 +0530

    completed

---
+++ Day_10_DSA Contest - Arrays and Strings/PatternOfIndexes.js
@@ -0,0 +1,55 @@
+Pattern of Indexes 
+
+Description
+
+You need to print pattern as given in Sample Input.
+Analyse the sample test case to understand the pattern.
+
+
+Input
+First line contains N. (1<= N <= 10)
+
+Output
+Print pattern for N
+
+Sample Input 1 
+
+1
+Sample Output 1
+
+0
+Sample Input 2 
+
+5
+Sample Output 2
+
+0 1 0 3 0
+1 1 1 3 1
+2 1 2 3 2
+3 1 3 3 3
+4 1 4 3 4
+Hint
+
+// output as code :
+
+function patternIndex(N){
+    //write code here
+    for(let i=0;i<N;i++)
+    {
+        let bag="";
+        for(let j=0;j<N;j++)
+        {
+            if(j%2==0)
+            {
+                bag+=i+" ";
+            }
+            else
+            {
+                bag+=j+" ";
+            }
+        }
+        console.log(bag);
+    }
+}
+
+
